b7b0d8e12cc7fd513aa1d7d4b6b5289623f47797
ysmak <thegrandchicken@gmail.com>
2017-05-30 14:00:58 -0700

    Added Google sign-in flow

---
+++ app/src/main/java/com/example/eliaschang8/tigernewspaper/SignupActivity.java
@@ -0,0 +1,159 @@
+package com.example.eliaschang8.tigernewspaper;
+
+import android.app.ProgressDialog;
+import android.content.Intent;
+import android.support.annotation.NonNull;
+import android.support.v7.app.AppCompatActivity;
+import android.os.Bundle;
+import android.util.Log;
+import android.view.View;
+import android.widget.Button;
+import android.widget.EditText;
+import android.widget.TextView;
+import android.widget.Toast;
+
+import com.backendless.Backendless;
+import com.backendless.BackendlessUser;
+import com.backendless.async.callback.AsyncCallback;
+import com.backendless.exceptions.BackendlessFault;
+import com.google.android.gms.auth.api.Auth;
+import com.google.android.gms.auth.api.signin.GoogleSignInAccount;
+import com.google.android.gms.auth.api.signin.GoogleSignInOptions;
+import com.google.android.gms.auth.api.signin.GoogleSignInResult;
+import com.google.android.gms.common.ConnectionResult;
+import com.google.android.gms.common.SignInButton;
+import com.google.android.gms.common.api.GoogleApiClient;
+
+public class SignupActivity extends AppCompatActivity implements GoogleApiClient.OnConnectionFailedListener {
+
+    private EditText email;
+    private EditText password;
+    private Button signupButton;
+
+    public static String APPLICATION_ID = "455645A8-8F0A-B86E-FFC0-0E642E4F0B00";
+    public static String SECRET_KEY = "19E40444-F63B-9C7B-FF20-2550DC0CBD00";
+    public static final String TAG = SignupActivity.class.getSimpleName();
+    private static final int RC_SIGN_IN = 9001;
+
+    private GoogleApiClient mGoogleApiClient;
+    private TextView mStatusTextView;
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_signup);
+
+        Backendless.initApp(this, APPLICATION_ID, SECRET_KEY, "v1");
+
+        //Wire widgets
+        email = (EditText) findViewById(R.id.email);
+        password = (EditText) findViewById(R.id.password);
+        signupButton = (Button) findViewById(R.id.button_signup);
+        SignInButton gSignInButton = (SignInButton) findViewById(R.id.google_sign_in_button);
+
+        signupButton.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View v) {
+                registerUser();
+                //TODO: Add intent to travel to [whatever] page
+            }
+        });
+
+        gSignInButton.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View v) {
+                switch (v.getId()) {
+                    case R.id.google_sign_in_button:
+                        signIn();
+                        break;
+                }
+            }
+
+            private void signIn() {
+                Intent signInIntent = Auth.GoogleSignInApi.getSignInIntent(mGoogleApiClient);
+                startActivityForResult(signInIntent, RC_SIGN_IN);
+            }
+        });
+
+        // Configure sign-in to request the user's ID, email address, and basic
+        // profile. ID and basic profile are included in DEFAULT_SIGN_IN.
+        GoogleSignInOptions gso = new GoogleSignInOptions.Builder(GoogleSignInOptions.DEFAULT_SIGN_IN)
+                .requestEmail()
+                .build();
+
+        mGoogleApiClient = new GoogleApiClient.Builder(this)
+                .enableAutoManage(this /* FragmentActivity */, this /* OnConnectionFailedListener */)
+                .addApi(Auth.GOOGLE_SIGN_IN_API, gso)
+                .build();
+
+        //TODO: Start sign-in flow
+        // https://developers.google.com/identity/sign-in/android/sign-in?configured=true
+
+
+    }
+
+    private void registerUser() {
+
+        BackendlessUser user = new BackendlessUser();
+        Log.d(TAG, "registerUser: User: " + (user == null));
+        //Reporting false so user is created, not null
+        user.setEmail(email.getText().toString());
+        Log.d(TAG, "registerUser: Password: " + (password == null));
+        user.setPassword(password.getText().toString());
+        //Log.d(TAG, "registerUser: after " + password.getText());
+
+        Backendless.UserService.register(user, new AsyncCallback<BackendlessUser>() {
+            public void handleResponse(BackendlessUser registeredUser) {
+                // user has been registered and now can login
+                Log.d(TAG, "onCreate: User registration successful");
+                Toast.makeText(SignupActivity.this, "User registration successful!",
+                        Toast.LENGTH_SHORT).show();
+            }
+
+            public void handleFault(BackendlessFault fault) {
+                // an error has occurred, the error code can be retrieved with fault.getCode()
+                Log.e(TAG, "onCreate: User registration FAILED: " + fault.getCode());
+            }
+        });
+    }
+
+    @Override
+    protected void onActivityResult(int requestCode, int resultCode, Intent data) {
+        super.onActivityResult(requestCode, resultCode, data);
+        // Result returned from launching the Intent from GoogleSignInApi.getSignInIntent(...);
+        if (requestCode == RC_SIGN_IN) {
+            GoogleSignInResult result = Auth.GoogleSignInApi.getSignInResultFromIntent(data);
+            handleSignInResult(result);
+        }
+    }
+
+    private void handleSignInResult(GoogleSignInResult result) {
+        Log.d(TAG, "handleSignInResult:" + result.isSuccess());
+        if (result.isSuccess()) {
+            // Signed in successfully, show authenticated UI.
+            GoogleSignInAccount acct = result.getSignInAccount();
+            mStatusTextView.setText(getString(R.string.signed_in_fmt, acct.getDisplayName()));
+            updateUI(true);
+        } else {
+            // Signed out, show unauthenticated UI.
+            updateUI(false);
+        }
+    }
+
+    @Override
+    public void onConnectionFailed(ConnectionResult connectionResult) {
+        // An unresolvable error has occurred and Google APIs (including Sign-In) will not
+        // be available.
+        Log.d(TAG, "onConnectionFailed:" + connectionResult);
+    }
+
+    private void updateUI(boolean signedIn) {
+
+        if (signedIn) {
+            findViewById(R.id.google_sign_in_button).setVisibility(View.GONE);
+        } else {
+            mStatusTextView.setText(R.string.signed_out);
+            findViewById(R.id.google_sign_in_button).setVisibility(View.VISIBLE);
+        }
+    }
+}
