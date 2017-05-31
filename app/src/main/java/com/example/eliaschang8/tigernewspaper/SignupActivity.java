d562d12e45fd801e423413264d065252474f6cbe
ysmak <thegrandchicken@gmail.com>
2017-05-30 17:24:27 -0700

    Added Google sign-in flow Messed up something with revoking access/signing out

--- app/src/main/java/com/example/eliaschang8/tigernewspaper/SignupActivity.java
+++ app/src/main/java/com/example/eliaschang8/tigernewspaper/SignupActivity.java
@@ -1,8 +1,6 @@
 package com.example.eliaschang8.tigernewspaper;
 
-import android.app.ProgressDialog;
 import android.content.Intent;
-import android.support.annotation.NonNull;
 import android.support.v7.app.AppCompatActivity;
 import android.os.Bundle;
 import android.util.Log;
@@ -23,6 +21,8 @@ import com.google.android.gms.auth.api.signin.GoogleSignInResult;
 import com.google.android.gms.common.ConnectionResult;
 import com.google.android.gms.common.SignInButton;
 import com.google.android.gms.common.api.GoogleApiClient;
+import com.google.android.gms.common.api.ResultCallback;
+import com.google.android.gms.common.api.Status;
 
 public class SignupActivity extends AppCompatActivity implements GoogleApiClient.OnConnectionFailedListener {
 
@@ -50,6 +50,9 @@ public class SignupActivity extends AppCompatActivity implements GoogleApiClient
         password = (EditText) findViewById(R.id.password);
         signupButton = (Button) findViewById(R.id.button_signup);
         SignInButton gSignInButton = (SignInButton) findViewById(R.id.google_sign_in_button);
+        Button signOutButton = (Button) findViewById(R.id.sign_out_button);
+        Button disconnectButton = (Button) findViewById(R.id.disconnect_button);
+        mStatusTextView = (TextView) findViewById(R.id.status);
 
         signupButton.setOnClickListener(new View.OnClickListener() {
             @Override
@@ -62,16 +65,33 @@ public class SignupActivity extends AppCompatActivity implements GoogleApiClient
         gSignInButton.setOnClickListener(new View.OnClickListener() {
             @Override
             public void onClick(View v) {
+               switch (v.getId()) {
+                        case R.id.google_sign_in_button:
+                            signIn();
+                            break;
+                }
+            }
+        });
+
+        signOutButton.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View v) {
                 switch (v.getId()) {
-                    case R.id.google_sign_in_button:
-                        signIn();
+                    case R.id.sign_out_button:
+                        signOut();
                         break;
                 }
             }
+        });
 
-            private void signIn() {
-                Intent signInIntent = Auth.GoogleSignInApi.getSignInIntent(mGoogleApiClient);
-                startActivityForResult(signInIntent, RC_SIGN_IN);
+        disconnectButton.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View v) {
+                switch (v.getId()) {
+                    case R.id.disconnect_button:
+                        revokeAccess();
+                        break;
+                }
             }
         });
 
@@ -86,10 +106,13 @@ public class SignupActivity extends AppCompatActivity implements GoogleApiClient
                 .addApi(Auth.GOOGLE_SIGN_IN_API, gso)
                 .build();
 
-        //TODO: Start sign-in flow
-        // https://developers.google.com/identity/sign-in/android/sign-in?configured=true
+        //Start sign-in flow, https://developers.google.com/identity/sign-in/android/sign-in?configured=true
+    }
 
-
+    @Override
+    protected void onStart() {
+        mGoogleApiClient.connect();
+        super.onStart();
     }
 
     private void registerUser() {
@@ -117,16 +140,6 @@ public class SignupActivity extends AppCompatActivity implements GoogleApiClient
         });
     }
 
-    @Override
-    protected void onActivityResult(int requestCode, int resultCode, Intent data) {
-        super.onActivityResult(requestCode, resultCode, data);
-        // Result returned from launching the Intent from GoogleSignInApi.getSignInIntent(...);
-        if (requestCode == RC_SIGN_IN) {
-            GoogleSignInResult result = Auth.GoogleSignInApi.getSignInResultFromIntent(data);
-            handleSignInResult(result);
-        }
-    }
-
     private void handleSignInResult(GoogleSignInResult result) {
         Log.d(TAG, "handleSignInResult:" + result.isSuccess());
         if (result.isSuccess()) {
@@ -140,6 +153,61 @@ public class SignupActivity extends AppCompatActivity implements GoogleApiClient
         }
     }
 
+    private void signIn() {
+        Intent signInIntent = Auth.GoogleSignInApi.getSignInIntent(mGoogleApiClient);
+        startActivityForResult(signInIntent, RC_SIGN_IN);
+    }
+
+    private void signOut() {
+        if (mGoogleApiClient.isConnected())
+        {
+            Auth.GoogleSignInApi.signOut(mGoogleApiClient).setResultCallback(
+                    new ResultCallback<Status>() {
+                        @Override
+                        public void onResult(Status status) {
+                            Log.d(TAG, "Sign out using Google API");
+                            updateUI(false);
+                        }
+                    });
+        }
+    }
+
+    private void revokeAccess() {
+        if (mGoogleApiClient.isConnected())
+        {
+            Auth.GoogleSignInApi.revokeAccess(mGoogleApiClient).setResultCallback(
+                    new ResultCallback<Status>() {
+                        @Override
+                        public void onResult(Status status) {
+                            Log.d(TAG, "Revoke access using Google API");
+                            updateUI(false);
+                        }
+                    });
+        }
+    }
+
+    private void updateUI(boolean signedIn) {
+
+        if (signedIn) {
+            findViewById(R.id.google_sign_in_button).setVisibility(View.GONE);
+            findViewById(R.id.sign_out_and_disconnect).setVisibility(View.VISIBLE);
+        } else {
+            mStatusTextView.setText("");
+            findViewById(R.id.google_sign_in_button).setVisibility(View.VISIBLE);
+            findViewById(R.id.sign_out_and_disconnect).setVisibility(View.GONE);
+        }
+    }
+
+    @Override
+    protected void onActivityResult(int requestCode, int resultCode, Intent data) {
+        super.onActivityResult(requestCode, resultCode, data);
+        // Result returned from launching the Intent from GoogleSignInApi.getSignInIntent(...);
+        if (requestCode == RC_SIGN_IN) {
+            GoogleSignInResult result = Auth.GoogleSignInApi.getSignInResultFromIntent(data);
+            handleSignInResult(result);
+        }
+    }
+
     @Override
     public void onConnectionFailed(ConnectionResult connectionResult) {
         // An unresolvable error has occurred and Google APIs (including Sign-In) will not
@@ -147,13 +215,5 @@ public class SignupActivity extends AppCompatActivity implements GoogleApiClient
         Log.d(TAG, "onConnectionFailed:" + connectionResult);
     }
 
-    private void updateUI(boolean signedIn) {
-
-        if (signedIn) {
-            findViewById(R.id.google_sign_in_button).setVisibility(View.GONE);
-        } else {
-            mStatusTextView.setText(R.string.signed_out);
-            findViewById(R.id.google_sign_in_button).setVisibility(View.VISIBLE);
-        }
-    }
+    //TODO: Figure out why the heck it isn't disconnecting/signing out/revoking access (SMH)
 }
